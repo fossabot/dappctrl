f7632125da49cb11c6ef5ebd0910e2a4dac9308a
Simon Prykhodko <semion.ababo@gmail.com>
2018-01-31 17:20:53 +0100

    Initial data schema.

--- data/schema.go
+++ data/schema.go
@@ -2,7 +2,47 @@ package data
 
 //go:generate reform
 
-//reform:users
-type User struct {
+//reform:services
+type Service struct {
 	ID string `reform:"id,pk"`
+	SO string `reform:"so"`
+}
+
+//reform:vpn_services
+type VPNService struct {
+	ServiceID string `reform:"service_id,pk"`
+}
+
+//reform:clients
+type Client struct {
+	ID string `reform:"id,pk"`
+}
+
+//reform:payments
+type Payment struct {
+	ID        string `reform:"id,pk"`
+	ServiceID string `reform:"service_id"`
+	ClientID  string `reform:"client_id"`
+	EthBlock  int    `reform:"eth_block"`
+	Solt      int64  `reform:"solt"`
+	Password  string `reform:"password"`
+}
+
+//reform:vpn_payments
+type VPNPayment struct {
+	PaymentID string `reform:"payment_id,pk"`
+	TotalMiBs int    `reform:"total_mibs"`
+}
+
+//reform:sessions
+type Session struct {
+	ID      string     `reform:"id,pk"`
+	Started time.Time  `reform:"started"`
+	Ended   *time.Time `reform:"ended"`
+}
+
+//reform:vpn_sessions
+type VPNSession struct {
+	SessionID    string `reform:"session_id,pk"`
+	ConsumedMiBs int    `reform:"consumed_mibs"`
 }
--- data/schema.sql
+++ data/schema.sql
@@ -1,3 +1,41 @@
-CREATE TABLE users (
-    id uuid
+CREATE DOMAIN eth_addr AS bytea CHECK(octet_length(value) = 20);
+CREATE DOMAIN sha3_256 AS bytea CHECK(octet_length(value) = 32);
+
+CREATE TABLE services (
+    id sha3_256 PRIMARY KEY,
+    so json NOT NULL
+);
+
+CREATE TABLE vpn_services (
+    service_id sha3_256 PRIMARY KEY REFERENCES services (id)
+);
+
+CREATE TABLE clients (
+    id eth_addr PRIMARY KEY
+);
+
+CREATE TABLE payments (
+    id sha3_256 PRIMARY KEY,
+    service_id sha3_256 NOT NULL REFERENCES services (id),
+    client_id eth_addr NOT NULL REFERENCES clients (id),
+    eth_block int NOT NULL,
+    solt bigint NOT NULL,
+    password sha3_256 NOT NULL
+);
+
+CREATE TABLE vpn_payments (
+    payment_id sha3_256 PRIMARY KEY REFERENCES payments (id),
+    total_mibs int NOT NULL
+);
+
+CREATE TABLE sessions (
+    id uuid PRIMARY KEY,
+    payment_id sha3_256 NOT NULL REFERENCES payments (id),
+    started timestamp NOT NULL,
+    ended timestamp
+);
+
+CREATE TABLE vpn_sessions (
+    session_id uuid PRIMARY KEY REFERENCES sessions (id),
+    consumed_mibs int
 );
