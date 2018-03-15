4577b4040ad26951779de601c6f8c6454a098ab0
Dima Chizhevsky <dima.chizhevsky@gmail.com>
2018-03-15 05:25:26 +0200

    transaction receipt getter

--- eth/lib/common.go
+++ eth/lib/common.go
@@ -1,5 +1,9 @@
 package lib
 
+import (
+	"strings"
+)
+
 // This module provides low-level methods for accessing common ethereum info.
 // For detailed API description, please refer to:
 // https://ethereumbuilders.gitbooks.io/guide/content/en/ethereum_json_rpc.html
@@ -9,12 +13,9 @@ type GasPriceAPIResponse struct {
 	Result string `json:"result"`
 }
 
-// Returns the current price per gas in wei.
-//
+// GetGasPrice returns current gas price in wei.
 // For the details, please, refer to:
 // https://ethereumbuilders.gitbooks.io/guide/content/en/ethereum_json_rpc.html#eth_gasprice
-//
-// Tests: common_test/TestGasPriceFetching
 func (e *EthereumClient) GetGasPrice() (*GasPriceAPIResponse, error) {
 	response := &GasPriceAPIResponse{}
 	return response, e.fetch("eth_gasPrice", "", response)
@@ -24,13 +25,30 @@ func (e *EthereumClient) GetGasPrice() (*GasPriceAPIResponse, error) {
 
 type BlockNumberAPIResponse GasPriceAPIResponse
 
-// Returns the number of most recent block in blockchain.
-//
+// GetBlockNumber returns the number of most recent block in blockchain.
 // For the details, please, refer to:
 // https://ethereumbuilders.gitbooks.io/guide/content/en/ethereum_json_rpc.html#eth_blocknumber
-//
-// Tests: common_test/TestBlockNumberFetching
 func (e *EthereumClient) GetBlockNumber() (*BlockNumberAPIResponse, error) {
 	response := &BlockNumberAPIResponse{}
 	return response, e.fetch("eth_blockNumber", "", response)
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+type TransactionReceiptAPIResponse struct {
+	apiResponse
+	Result string `json:"result"`
+}
+// GetTransactionReceipt returns receipt of the transaction,
+// specified by the hash.
+// https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_gettransactionreceipt
+func (e *EthereumClient) GetTransactionReceipt(hash string) (*TransactionReceiptAPIResponse, error) {
+	if len(hash) > 2 {
+		if strings.ToLower(hash[:2]) != "0x" {
+			hash = "0x" + hash
+		}
+	}
+
+	response := &TransactionReceiptAPIResponse{}
+	return response, e.fetch("eth_getTransactionReceipt", `"` + hash + `"`, response)
 }
